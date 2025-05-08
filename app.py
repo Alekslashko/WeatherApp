805df00aea50dd7d860e94a263b7883e560e6b5e
Alekslashko <alexandarshou21a@gmail.com>
2025-05-08 12:51:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}
 response = requests.get(URL)
 data = response.json()
 
-st.write({data})
+st.write({data["weather"]})
